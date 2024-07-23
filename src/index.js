d85aed01f3944bef09e789732c041e07964ff509
Yashd03 <dubeyyash6602@gmail.com>
2024-07-23 21:39:23 +0530

    Api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 
 
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './.env'
 })
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.Port ||8000,()=>{
+        console.log(`server is running at ${process.env.Port}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed",err)
+})
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,10 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message="something went wrong",
+        Error=[],
+        stack=""
+    ){
+        
+    }
+}
